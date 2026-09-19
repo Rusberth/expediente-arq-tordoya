bc0de82d5f1ef4c484fca958efebc3021cf24d22
Rusberth <39111011+Rusberth@users.noreply.github.com>
2026-09-19 13:59:06 -0400

    producto

---
+++ H3/con-observer/Producto.cs
@@ -0,0 +1,8 @@
+namespace ElAhorro.ConObserver;
+
+public class Producto
+{
+    public string Nombre { get; init; } = "";
+    public int StockActual { get; set; }
+    public int StockMinimo { get; init; }
+}
